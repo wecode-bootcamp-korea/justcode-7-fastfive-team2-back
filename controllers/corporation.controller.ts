6b51ff63d962d174c4880e72d56d2081f4160564
Kimseonghee <kimsunghee625@gmail.com>
2022-12-16 02:58:25 +0900

    FEAT: value값 없을시 에러처리 모듈화 추가 및 적용

--- controllers/corporation.controller.ts
+++ controllers/corporation.controller.ts
@@ -1,10 +1,15 @@
 import { Request, Response } from "express";
 import corporationService from "../services/corporation.service";
+import { checkDataIsNotEmpty } from "../util/myutil";
 
 const findCorporationDetail = async (req: Request, res: Response) => {
   try {
     const corporationId = req.params.id;
+
+    checkDataIsNotEmpty(corporationId);
+
     let [data] = await corporationService.findCorporationDetail(corporationId);
+
     res.status(200).json({ message: "success", data });
   } catch (err: any) {
     console.log(err);
